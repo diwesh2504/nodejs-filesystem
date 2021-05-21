4b4a77a1f92a930ccb9fdeb71c2eac8dc6cbbc6c
diwesh2504 <66382963+diwesh2504@users.noreply.github.com>
2021-05-21 20:08:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,6 +36,6 @@ app.get("/readfolder",function (req,res){
     });
     
 })
-app.listen(proccess.env.PORT||3000,()=>{
+app.listen(process.env.PORT||3000,()=>{
     console.log("Backend start");
 })
